eea8b2b2f159200d0855a3c3af644370a7ac44c9
daniel-1408-1 <2008danielha@gmail.com>
2025-08-05 10:04:12 -0400

    Update PlatformerController.cs

--- Vestige/Assets/Assets/Scripts/Player/PlatformerController.cs
+++ Vestige/Assets/Assets/Scripts/Player/PlatformerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlatformController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private float horizontalInput;
